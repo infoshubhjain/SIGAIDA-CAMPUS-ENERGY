f6214f94bb23ccef8d24eb60f4c2724e7874ae28
Junhao Zhang <jerry_mountain@hotmail.com>
2025-10-18 15:32:22 -0500

    Initial Commit

--- sigaida-campus-energy/src/app/page.js
+++ sigaida-campus-energy/src/app/page.js
@@ -1,10 +1,32 @@
-import Link from 'next/link';
-import { Button } from '@mantine/core';
+'use client';
+
+import { AppShell, Burger, Group, Menu, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 
 export default function Home() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <Button component={Link} href="/hello">
-      Next link button
-    </Button>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group>
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Text>SIGAIDA Campus Energy</Text>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        <Menu>
+          <Menu.Item>Test</Menu.Item>
+        </Menu>
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <Text>This is the main section, your app content here.</Text>
+        <Text>Layout used in most cases – Navbar and Header with fixed position</Text>
+      </AppShell.Main>
+    </AppShell>
   );
 }
